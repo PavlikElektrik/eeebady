0757a798fc2dfabb11f7a23f5e2851c9e29760b8
PavlikElektrik <152876168+PavlikElektrik@users.noreply.github.com>
2025-09-02 15:44:52 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (45.15%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.py
+++ 3379-score-of-a-string/score-of-a-string.py
@@ -1,5 +1,4 @@
 class Solution:
-    #aboba
     def scoreOfString(self, s: str) -> int:
         ascii_values = [ord(char) for char in s]
         return sum(abs(ascii_values[i] - ascii_values[i+1]) for i in range(len(s)-1))
